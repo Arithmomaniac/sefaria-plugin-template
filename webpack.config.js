8a4cb857a8b7060f41bdd4a46a01ce02205b82b7
Avi Levin <avilevin@microsoft.com>
2025-04-25 10:58:38 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // new import
+
+module.exports = {
+  entry: './plugin.ts',
+  output: {
+    filename: 'plugin.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html', // outputs to dist/index.html
+      inject: 'head'
+    })
+  ],
+  mode: 'development',
+  devtool: 'inline-source-map', // Added for better debugging
+  devServer: {
+    static: './dist', // Serve files from the dist directory
+    hot: true, // Enable Hot Module Replacement
+    open: true // Automatically open the browser
+  }
+};
